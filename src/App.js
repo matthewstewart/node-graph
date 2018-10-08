ed6149e564537c97848752eb00eba7e5acf67d2a
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-10-08 13:27:20 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ForceGraph2D, ForceGraph3D, ForceGraphVR } from 'react-force-graph';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="2D Graph">
+          <ForceGraph2D 
+            graphData={genRandomTree()} 
+            style={{'height': '600px'}}
+          />
+        </div>
+        <div id="3D Graph">
+          <ForceGraph3D 
+            graphData={genRandomTree()} 
+            style={{'height': '600px'}}
+          />
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+function genRandomTree(nodeCount=300) {
+  return {
+    nodes: [...Array(nodeCount).keys()].map(i => ({ id: i })),
+      links: [...Array(nodeCount).keys()]
+    .filter(id => id)
+    .map(id => ({
+      source: id,
+      target: Math.round(Math.random() * (id-1))
+    }))
+  };  
+}
